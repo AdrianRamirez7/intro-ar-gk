90f388ea7898797119f883499fa604e9049d634f
AdrianRamirez7 <adrian.ramirez45679@gmail.com>
2023-08-31 01:38:57 -0700

    array js files for 08/31/23

---
+++ adrian-arrays.js
@@ -0,0 +1,9 @@
+//Consider The Variable 
+var groceryList = ["chips", "dip", "cookies"];
+
+// Write the code that will add "soda" to the end of the original array.
+groceryList.push()
+// Write the code that will add "granola" to the end of the array without altering the original array.
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+// Write the code that will add "beans" to the "chips" and "dip" array.
+
